ccb6571866bdc4f84d6b13261552cb632d240af4
unknown <pdpbkpukotabengkulu@gmail.com>
2026-06-07 00:53:37 +0700

    update app

--- app.py
+++ app.py
@@ -611,13 +611,6 @@ if menu == "Dashboard":
 
     # ================= LOAD DATA =================
     df = load_data()
-    st.write(df[df["Lokasi"] == "Rumah"][["Nama","Durasi","Lokasi"]].tail(10))
-
-    st.write(
-        df[df["Lokasi"] == "Rumah"]["Durasi"]
-        .apply(lambda x: f"{x} ({type(x)})")
-        .tolist()
-    )
 
     st.write(df.columns.tolist())
     st.write(df.tail(3))
